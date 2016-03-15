683d84d6bd5fd98d918e00c8caba6e1d45a1bf2e
Rob Cornacchia <bobbycornacchia@gmail.com>
2016-03-14 22:20:39 -0400

    finished api

--- server.js
+++ server.js
@@ -30,6 +30,39 @@ app.get('/api/todos', function(req, res) {
     });
 });
 
+// POST - create a Todo
+app.post('/api/todos', function(req, res) {
+    Todo.create({
+        text: req.body.text,
+        done: false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+        // get and return all the todos after you create another
+        Todo.find(function(err, todos) {
+            if(err)
+                res.send(err);
+            res.json(todos);
+        });
+    });
+});
+
+// delete a post
+app.delete('/api/todos/:todo_id', function(req, res){
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if(err)
+            res.send(err);
+        // get and return all the todos after you delete one
+        Todo.find(function(err, todos) {
+            if(err)
+                res.send(err);
+            res.json(todos);
+        });
+    });
+});
+
 var Todo = mongoose.model('Todo', {
     text: String
 });
